b05ff64017e45c3d21d0395cf5013aa11c3c0c18
nitesla <gbeminiyiariyoagbaje@gmail.com>
2019-07-05 09:00:22 +0100

    index page

--- pet-clinic-web/src/main/java/nikola/tesla/petclinic/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/nikola/tesla/petclinic/PetClinicApplication.java
@@ -1,4 +1,4 @@
-package tesla.petclinic;
+package nikola.tesla.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- pet-clinic-web/src/main/java/nikola/tesla/petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/nikola/tesla/petclinic/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package tesla.petclinic.controllers;
+package nikola.tesla.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
